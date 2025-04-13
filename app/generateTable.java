3ee38496d955ffc773f71aed40d80688b865cb86
dolftenhave <dolftenhave@gmail.com>
2025-04-13 16:07:40 +1200

    Committing initial empty project with a vague plan of how the data will be generated

---
+++ app/generateTable.java
@@ -0,0 +1,9 @@
+/**
+ * @author Dolf ten Have
+ * @date 13/04/2025
+ * /
+public class generateTable{
+	public static void main(String args[]){
+
+	}
+}
---
+++ app/parseArgs.java
@@ -0,0 +1,9 @@
+/**
+ *	@author Dolf ten Have 
+ *	@date 13/04/2025
+ */
+public class parseArgs{
+	public parseArgs(String args[]){
+		return;
+	}
+}
