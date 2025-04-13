4af4650f66f8a1c9935ca83504edef6b2c66ac8c
dolftenhave <dolftenhave@gmail.com>
2025-04-13 19:46:45 +1200

    Created the parse args table maker that creates a table based on input parameters. This table will then be used by MakeCSV to create large csv datasets

--- app/README.md
+++ app/README.md
@@ -2,6 +2,21 @@
 
 This application is designed to generate large amounts of csv data.
 
-## usage
+## parseArgs
 
-## tags
+This program generates a pre defined table that will be used by MakeCSV to generate a bunch of random csv data
+
+#### Usage
+
+`parseArgs <table name> <arguments>`
+
+There are several different type of arguments used to greate the data
+
+- **varchar** `-v <length>` legnth 1 to n.
+- **integer** `-i <length>` lengtth 0 will auto increment starting form 1. A fixed lenth will generate a random number with that many digits *e.g. 2 will generate a number 10-99*
+- **date** `-d` will generate a random date in the format DD/MM/YYYY
+- **time** `-t` will generate a random time
+- **file** `-f <path to file> <column to choose the data from>` will take data from a specified column of another csv file and insert it into this column. This is used in instances of foreign keys.
+- **double** `-o <length> <delimiter position>` will generate a dounle with the length and position of the delimiter.
+
+## MakeCSV
--- app/parseArgs.java
+++ app/parseArgs.java
@@ -1,9 +1,148 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 /**
- *	@author Dolf ten Have 
- *	@date 13/04/2025
+ * @author Dolf ten Have
+ * @date 13/04/2025
+ *
+ *       Parses a set of input parameters which are then used by the this
+ *       program to
+ *       Create a table that is used by makeCSV to generate lines of csv data.
  */
-public class parseArgs{
-	public parseArgs(String args[]){
-		return;
+public class parseArgs {
+	private static String _args[];
+	private static int p = 1; // a pointer that keeps track of where in the _args array the program is
+	private static BufferedWriter out;
+
+	public static void main(String args[]) {
+		_args = args;
+		createTable();
+	}
+
+	private static void createTable() {
+
+		try {
+			out = new BufferedWriter(new FileWriter(_args[0] + "_table.txt"));
+			out.flush();
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+			System.exit(1);
+		}
+
+		try {
+			while (p < _args.length) {
+				// As long as the first argument isn't read, add a new line
+				if (p != 1) {
+					out.newLine();
+				}
+
+				switch (_args[p]) {
+					// varchar
+					case "-v":
+						varchar();
+						break;
+					// integer
+					case "-i":
+						int_();
+						break;
+					// date
+					case "-d":
+						date();
+						break;
+					// time
+					case "-t":
+						time();
+						break;
+					// file
+					case "-f":
+						file();
+						break;
+					// double
+					case "-o":
+						double_();
+						break;
+					// Unrecognised argument
+					default:
+						System.err.println("The input '" + _args[p] + "' was not int the correct format");
+						out.flush();
+						out.close();
+						System.exit(1);
+						break;
+				}
+				out.flush();
+			}
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+			System.exit(1);
+		}
+	}
+
+	/**
+	 * Writes a line to the buffered writer Output
+	 */
+	private static void write(String s) {
+		try {
+			out.write(s, 0, s.length());
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+			System.exit(1);
+		}
+	}
+
+	/**
+	 * Adds a line too the jump table which indicates type varchar and the length of
+	 * the varchar
+	 */
+	private static void varchar() {
+		write("0 ");
+		write(_args[p + 1]);
+		p += 2;
+	}
+
+	/**
+	 * Adds a line to the jump table wich indicates the type int and the length of
+	 * the int (0 is incremental)
+	 */
+	private static void int_() {
+		write("1 ");
+		write(_args[p + 1]);
+		p += 2;
+	}
+
+	/**
+	 * Adds a line to the jump table which indicated type date
+	 */
+	private static void date() {
+		write("2");
+		p++;
+	}
+
+	/**
+	 * Adds a line to the jump table which indicates type time
+	 */
+	private static void time() {
+		write("3");
+		p++;
+	}
+
+	/**
+	 * Adds a line to the jump table which indicates type file, followed by the
+	 * column of the data and then the directory of the file and the
+	 */
+	private static void file() {
+		write("4 ");
+		write(_args[p + 1] + " " + _args[p + 2]);
+		p += 3;
+	}
+
+	/**
+	 * Writes a line to the jump table which indicates the type double, followed by
+	 * the length of the double and where the seperator should be
+	 */
+	private static void double_() {
+		write("5 ");
+		write(_args[p + 1] + " " + _args[p + 2]);
+		p += 3;
 	}
 }
