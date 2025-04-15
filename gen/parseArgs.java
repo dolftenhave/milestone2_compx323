f798854621c354c98056fab2916dc8045386cf29
dolftenhave <dolftenhave@gmail.com>
2025-04-16 08:10:37 +1200

    Reformated the class to use a arraylist as a buffer before writing the data which allows me to add a head line to the file which indicates the length of the file and how many external file reffernces are used

--- gen/parseArgs.java
+++ gen/parseArgs.java
@@ -1,5 +1,6 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.util.ArrayList;
 
 /**
  * @author Dolf ten Have
@@ -14,76 +15,78 @@ public class parseArgs {
 	private static int p = 1; // a pointer that keeps track of where in the _args array the program is
 	private static BufferedWriter out;
 
+	private static int fileRefs = 0; // The number of external files refferenced for data.
+
+	private static ArrayList<String> table; // a "buffer" array that holds the table data before it is written;
+
 	public static void main(String args[]) {
 		_args = args;
+		table = new ArrayList<String>();
 		createTable();
 	}
 
 	private static void createTable() {
-
-		try {
-			out = new BufferedWriter(new FileWriter(_args[0] + "_table.txt"));
-			out.flush();
-		} catch (Exception e) {
-			e.printStackTrace(System.err);
-			System.exit(1);
-		}
-
-		try {
-			while (p < _args.length) {
-				// As long as the first argument isn't read, add a new line
-				if (p != 1) {
-					out.newLine();
-				}
-
-				switch (_args[p]) {
-					// varchar
-					case "-v":
-						varchar();
-						break;
-					// integer
-					case "-i":
-						int_();
-						break;
-					// date
-					case "-d":
-						date();
-						break;
-					// time
-					case "-t":
-						time();
-						break;
-					// file
-					case "-f":
-						file();
-						break;
-					// double
-					case "-o":
-						double_();
-						break;
-					// Unrecognised argument
-					default:
-						System.err.println("The input '" + _args[p] + "' was not int the correct format");
-						out.flush();
-						out.close();
-						System.exit(1);
-						break;
-				}
-				out.flush();
+		while (p < _args.length) {
+			switch (_args[p]) {
+				// varchar
+				case "-v":
+					varchar();
+					break;
+				// integer
+				case "-i":
+					int_();
+					break;
+				// date
+				case "-d":
+					date();
+					break;
+				// time
+				case "-t":
+					time();
+					break;
+				// file
+				case "-f":
+					file();
+					break;
+				// double
+				case "-o":
+					double_();
+					break;
+				// Unrecognised argument
+				default:
+					System.err.println("The input '" + _args[p] + "' was not int the correct format");
+					System.exit(1);
+					break;
 			}
-			out.close();
-		} catch (Exception e) {
-			e.printStackTrace(System.err);
-			System.exit(1);
 		}
+
+		writeTable();
 	}
 
 	/**
-	 * Writes a line to the buffered writer Output
+	 * Writes a head line to the table file with the size of the table ArrayList and
+	 * the number of external refferences made;
+	 * Then writes the content of table line for line to the file
 	 */
-	private static void write(String s) {
+	private static void writeTable() {
 		try {
-			out.write(s, 0, s.length());
+			out = new BufferedWriter(new FileWriter(_args[0] + "_table.txt"));
+
+			String head = table.size() + " " + fileRefs;
+			out.write(head, 0, head.length());
+			out.newLine();
+			out.flush();
+
+			for (int i = 0; i < table.size() - 1; i++) {
+				out.write(table.get(i), 0, table.get(i).length());
+				out.newLine();
+				out.flush();
+			}
+			out.write(table.get(table.size() - 1), 0, table.get(table.size() - 1).length());
+			out.flush();
+
+			out.close();
+
 		} catch (Exception e) {
 			e.printStackTrace(System.err);
 			System.exit(1);
@@ -95,8 +98,7 @@ public class parseArgs {
 	 * the varchar
 	 */
 	private static void varchar() {
-		write("0 ");
-		write(_args[p + 1]);
+		table.add("0 " + _args[p + 1]);
 		p += 2;
 	}
 
@@ -105,8 +107,7 @@ public class parseArgs {
 	 * the int (0 is incremental)
 	 */
 	private static void int_() {
-		write("1 ");
-		write(_args[p + 1]);
+		table.add("1 " + _args[p + 1]);
 		p += 2;
 	}
 
@@ -114,7 +115,7 @@ public class parseArgs {
 	 * Adds a line to the jump table which indicated type date
 	 */
 	private static void date() {
-		write("2");
+		table.add("2");
 		p++;
 	}
 
@@ -122,7 +123,7 @@ public class parseArgs {
 	 * Adds a line to the jump table which indicates type time
 	 */
 	private static void time() {
-		write("3");
+		table.add("3");
 		p++;
 	}
 
@@ -131,9 +132,9 @@ public class parseArgs {
 	 * column of the data and then the directory of the file and the
 	 */
 	private static void file() {
-		write("4 ");
-		write(_args[p + 1] + " " + _args[p + 2]);
+		table.add("4 " + _args[p + 1] + " " + _args[p + 2]);
 		p += 3;
+		fileRefs++;
 	}
 
 	/**
@@ -141,8 +142,7 @@ public class parseArgs {
 	 * the length of the double and where the seperator should be
 	 */
 	private static void double_() {
-		write("5 ");
-		write(_args[p + 1] + " " + _args[p + 2]);
+		table.add("5 " + _args[p + 1] + " " + _args[p + 2]);
 		p += 3;
 	}
 }
