67bbdb0fd0210502b26a480e94bb4f3694b62d8b
Dolf <dolftenhave@gmail.com>
2025-04-20 09:30:29 +1200

    Updated testing app

--- gen/makeTables.sh
+++ gen/makeTables.sh
@@ -3,6 +3,6 @@
 # @author Dolf ten Have
 # This script automates the creation of table files for compx323 milestone 2
 
-java parseArgs v -v -d -t -o 9000
-java MakeCSV 2000 v_table.txt out
+java parseArgs v -v 6 -d -t -o 7 9 -I 6 -V 2 -i 500
+java MakeCSV $1 v_table.txt out
 
