c24c04f88fd16505ceffe75fb3a63c001300e9fa
Dolf <dolftenhave@gmail.com>
2025-04-21 08:23:44 +1200

    Created a csv file container for the purpose of reading random csv lines

--- gen/csvFile.java
+++ gen/csvFile.java
@@ -1,55 +1,55 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.random;
+import java.util.Hashtable;
+import java.util.Random;
 
 class csvFile {
 
-	private int col[][];
-	Random rand;
-
-	private Arraylist<String[]> data;
+	private Hashtable<Integer, Integer> col;
+	private Random rand;
+	private int currentLine;
+	private ArrayList<String[]> data;
 
 	/**
 	 * Creates a new csvFile object that contains the data from the speciefied columns
 	 * @param path the path of the file 
 	 * @param col[] the columns of the file that you want to keep
 	 */
-	public  csvFile(String path, int col[]){
-		createPathTranslator(col[]);		
-		readAllData(path);
+	public csvFile(String path, int columns[]){
+		createPathTranslator(columns);		
+		readAllData(path,columns);
 	}
 
 	/**
 	 * Creates a pointer array that translate the requested column into the locally stored column index 
 	 */
 	private void createPathTranslator(int org[]){
-		col = new int[org.length][2];
+		col = new Hashtable<Integer, Integer>();
 		for(int i = 0; i < org.length; i++){
-			col[i][0] = org[i];
-			col[i][1] = i;
+			col.put(org[i],i);
 		}
 	}
 
 	/**
 	 * Reads all all the data of the csv file and stores the specified columns in an array
 	 */
-	private void readAllData(String path){
+	private void readAllData(String path, int columns[]){
 		try{
 			BufferedReader in = new BufferedReader(new FileReader(path));	
 			String line;
-			String split;
-			String save;
+			String split[];
+			String save[];
 			boolean eof = false;
-			data = new ArrayList<String[]>()
+			data = new ArrayList<String[]>();
 
 			while(eof){
 				line = in.readLine();
 				if(line != null){
 					split = line.split(",");	
-					save = new String[col.length];
-					for(int i = 0; i < col.length; i++){
-						save[i] = line[col[i][0]];
+					save = new String[col.size()];
+					for(int i = 0; i < col.size(); i++){
+						save[i] = split[col.get(columns[i])];
 					}
 					data.add(save);
 				}else{
@@ -59,9 +59,14 @@ class csvFile {
 		}catch(Exception e){
 			e.printStackTrace(System.err);
 	}
-
-	private static int reverseTranslate(int i){
-		return col[
+	
+	/**
+	 * Returns a random element from the speciefied row
+	 * @param column the column in the csv file the data is in 
+	 */
+	 }
+	public String getRandomLine(int column){
+		String line[] = data.get(rand.nextInt(data.size()));
+		return line[col.get(column)];
 	}
-
 }
