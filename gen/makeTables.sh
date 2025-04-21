2187f0934467a51b62685164706039126ce1ce22
dolftenhave <dolftenhave@gmail.com>
2025-04-21 21:02:43 +1200

    Final commit of completed data gen project

--- gen/makeTables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# @author Dolf ten Have
-# This script automates the creation of table files for compx323 milestone 2
-
-java parseArgs v -v 6 -d -t -o 7 9 -I 6 -V 2 -i 500 -f out2.csv 0 -F out2.csv 0 -I 0
-java MakeCSV $1 v_table.txt out
-
