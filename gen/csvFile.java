0e8c81969e1c62885b20b8b99f1ba10da32725aa
dolftenhave <dolftenhave@gmail.com>
2025-04-21 20:49:56 +1200

    Finished commenting and ran my formatter to make the code look tidy

--- gen/csvFile.java
+++ gen/csvFile.java
@@ -4,40 +4,56 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Random;
 
+/**
+ * @author Dolf ten Have
+ * @date 21/04/2025
+ *
+ *       A container class that stores specified columns of data from a csv file
+ *       for use in the random csv data project
+ */
 class csvFile {
 
 	private Hashtable<Integer, Integer> col;
 	private Random rand;
-	private int currentLine;
 	private ArrayList<String[]> data;
 
 	/**
-	 * Creates a new csvFile object that contains the data from the speciefied columns
-	 * @param path the path of the file 
-	 * @param col[] the columns of the file that you want to keep
+	 * Creates a new csvFile object that contains the data from the speciefied
+	 * columns
+	 * 
+	 * @param path    the path of the file
+	 * @param columns the columns of the file that you want to keep
 	 */
-	public csvFile(String path, int columns[]){
+	public csvFile(String path, int columns[]) {
 		rand = new Random();
-		createPathTranslator(columns);		
-		readAllData(path,columns);
+		createPathTranslator(columns);
+		readAllData(path, columns);
 	}
 
 	/**
-	 * Creates a pointer array that translate the requested column into the locally stored column index 
+	 * Creates a pointer array that translate the requested column into the locally
+	 * stored column index
+	 * 
+	 * @param org the original column indexes of the data in the csv file
 	 */
-	private void createPathTranslator(int org[]){
+	private void createPathTranslator(int org[]) {
 		col = new Hashtable<Integer, Integer>();
-		for(int i = 0; i < org.length; i++){
-			col.put(org[i],i);
+		for (int i = 0; i < org.length; i++) {
+			col.put(org[i], i);
 		}
 	}
 
 	/**
-	 * Reads all all the data of the csv file and stores the specified columns in an array
+	 * Reads all all the data of the csv file and stores the specified columns in an
+	 * array
+	 * 
+	 * @param path    path to the csv file
+	 * @param columns the columns in the csv file that will be stored in this
+	 *                container
 	 */
-	private void readAllData(String path, int columns[]){
-		try{
-			BufferedReader in = new BufferedReader(new FileReader(path));	
+	private void readAllData(String path, int columns[]) {
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(path));
 			String line;
 			String split[];
 			String save[];
@@ -45,33 +61,34 @@ class csvFile {
 			int lines = 0;
 			data = new ArrayList<String[]>();
 
-			while(!eof){
+			while (!eof) {
 				line = in.readLine();
-				if(line != null){
-					split = line.split(",");	
+				if (line != null) {
+					split = line.split(",");
 					save = new String[col.size()];
-					for(int i = 0; i < col.size(); i++){
+					for (int i = 0; i < col.size(); i++) {
 						save[i] = split[col.get(columns[i])];
 					}
 					data.add(save);
 					lines++;
-				}else{
+				} else {
 					eof = true;
 				}
 			}
 			System.out.println("csvFile: read " + lines);
 			in.close();
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace(System.err);
 			System.exit(1);
 		}
 	}
-	
+
 	/**
 	 * Returns a random element from the speciefied row
-	 * @param column the column in the csv file the data is in 
+	 * 
+	 * @param column the column in the csv file the data is in
 	 */
-	public String getRandomLine(int column){
+	public String getRandomLine(int column) {
 		String line[] = data.get(rand.nextInt(data.size()));
 		return line[col.get(column)];
 	}
