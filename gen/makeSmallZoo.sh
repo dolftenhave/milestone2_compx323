28abe8eec898669c9b25881d3e3b2652f1ae9d0b
Ten Have <dt194@lab-rg07-30.cms.waikato.ac.nz>
2025-05-02 13:58:53 +1200

    Updated tables to now use the the new header arguments

--- gen/makeSmallZoo.sh
+++ gen/makeSmallZoo.sh
@@ -5,7 +5,7 @@
 # Initialization of all the table files
 java parseArgs SpeciesGroup lastname commonName -V 10 -v 30
 java parseArgs Zone name colour hexCode -V 10 -v 15 -h 6
-java parseArgs Enclosure eid biome eSize zoneName -I 1 -v 15 -i 4 -F "csv/Zone.csv" 0
+java parseArgs Enclosure eid biome eSize zoneName -I 1 -v 15 -i 4 -F "csv/Zone.csv" 0 
 java parseArgs Species latinName commonName requiredBiome speciesGroup -V 20 -v 30 -f "csv/Enclosure.csv" 2 -F "csv/SpeciesGroup.csv" 0
 java parseArgs Staff sid fName lName dob phNumber email streetNumber streetName suburb city postCode clinic sex -I 1 -v 6 -v 10 -d -p -e -i 3 -v 6 -v 7 -v 8 -i 4 -v 1 -g
 java parseArgs Animal aid sex feedingInterval name weight originCountry dob enclosureID speciesName -I 1 -g -i 2 -v 8 -o 10 2 -f "csv/slim-3.csv" 1 -d -F "csv/Enclosure.csv" 0 -F "csv/Species.csv" 0
@@ -13,15 +13,14 @@ java parseArgs Oversees sGroupName staffID -F "csv/SpeciesGroup.csv" 0 -F "csv/S
 java parseArgs Care staffID animalID dateTime care notes -F "csv/Staff.csv" 0 -F "csv/Animal.csv" 0 -T -F "csv/care.csv" 0 -v 20
 java parseArgs Feed staffID animalID dateTime amount foodType -F "csv/Staff.csv" 0 -F "csv/Animal.csv" 0 -T -o 5 2 -F "csv/food.csv" 0
 
-
 # Creation of mock data
-java MakeCSV 10000 "tables/SpeciesGroup.tab" SpeciesGroup
-java MakeCSV 5000 "tables/Zone.tab" Zone
-java MakeCSV 60000 "tables/Enclosure.tab" Enclosure
-java MakeCSV 30000 "tables/Species.tab" Species
-java MakeCSV 50000 "tables/Animal.tab" Animal
-java MakeCSV 20000 "tables/Staff.tab" Staff
-java MakeCSV 100000 "tables/Oversees.tab" Oversees
-java MakeCSV 200000 "tables/Care.tab" Care
-java MakeCSV 500000 "tables/Feed.tab" Feed
+java MakeCSV 10 "tables/SpeciesGroup.tab" SpeciesGroup
+java MakeCSV 5 "tables/Zone.tab" Zone
+java MakeCSV 20 "tables/Enclosure.tab" Enclosure
+java MakeCSV 20 "tables/Species.tab" Species
+java MakeCSV 30 "tables/Animal.tab" Animal
+java MakeCSV 10 "tables/Staff.tab" Staff
+java MakeCSV 30 "tables/Oversees.tab" Oversees
+java MakeCSV 50 "tables/Care.tab" Care
+java MakeCSV 50 "tables/Feed.tab" Feed
 # total 957,000 lines
